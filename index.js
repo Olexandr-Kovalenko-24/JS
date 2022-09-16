c6e6f1c995e5e439fe726aef7b484579db725282
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-16 16:40:06 +0300

    Primitive 6 type

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-let box;
-let newBox = 3;
-
-box = 5;
-box = true;
-box = 'Hello'
-
-console.log(box);
-
-let value = 'Very important value';
-
-const a = 5;
+let box = 3;
+let box2 = '3';
+let box3 = true;
+let box4 = null;
+let box5 = undefined;
+let box6 = 2n;
