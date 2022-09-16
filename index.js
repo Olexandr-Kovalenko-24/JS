671e381abf00d16b423433d23aa9542a105c5270
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-16 15:38:26 +0300

    Primitive values

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log('Hello, world!');
+// window.alert('Hello!');
+// confirm('Are you 18?')
+// alert('Hello!');
+// alert(true);
+console.log(34);
+console.log('text');
+console.log(true);
+console.log(null);
+console.log(undefined);
+console.log(1n);
