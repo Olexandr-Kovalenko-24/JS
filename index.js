c6309f9b68630771a4b19882766b874a29e8664c
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-19 16:40:11 +0300

    Scope

--- index.js
+++ index.js
@@ -14,15 +14,23 @@
 // console.log(res2);
 
 
-function getBigOfTwoNumbers(a, b) {
-    debugger;
-if (a > b) {
-    return a;
-}
-else {
-    return b
-}
-}
+// function getBigOfTwoNumbers(a, b) {
+//     debugger;
+// if (a > b) {
+//     return a;
+// } else {
+//     return b
+// }
+// }
 
-const res = getBigOfTwoNumbers(4, 5);
-console.log(res);
+// const res = getBigOfTwoNumbers(4, 5);
+// console.log(res);
+
+const num = 5;
+
+function fn(){
+    // const num = 10;
+    debugger;
+    console.log(num);
+}
+fn()
