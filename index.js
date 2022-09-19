7a3f2b021740eaa61dc43b4dd1c43e91e1ec1345
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-19 14:32:23 +0300

    ||

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-let a = true;
-console.log(a);
-console.log(!a);
-//!true = false
-//!!true = true
-
-//<=
-//>=
-// true && false;
-// false
-// true && true;
-// true
-// false && false;
-// false
-// 1 && 1;
-// 1
-// 1 && 0
-// 0
-// 1 && true && 'test'
-// 'test'
-// 1 && false && 'test'
-// false
+true || true;
+true
+true || false;
+true
+false || false;
+false
