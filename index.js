dc1f3a7e3e00a42e8e6f3e7f996e1fdf66a639ed
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-19 15:06:35 +0300

    Modal windows

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-// let a = 42;
-// a += 3;
-// a -= 3;
-// a *= 3;
-// a /= 3;
+alert('Hello!');
 
-// let b=1, c=10 //Bad practice
+const res = confirm('Are you ok?');
+console.log(res);
 
-
-let a = 42;
-let b = 2;
-let c = 6;
-let d = 3;
-let e = 9;
-let f = 8;
-console.log(a*b);
-console.log(c/d);
-console.log(e+f);
-
-let g = 33;
-console.log(g);
-let l = false;
-console.log(l);
-let k = 'javasript';
-console.log(k);
-let m = 100;
-console.log(m);
-
-let num = 1;
-num++;
-num--;
-num += 11;
-num -= 11;
-num *= 11;
-num /= 11;
-console.log(num);
+const uservalue = prompt('How old are you?');
+console.log(uservalue);
