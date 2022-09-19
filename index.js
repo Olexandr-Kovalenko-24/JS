b38242a311027815802f8e2cb306bd3fd1299c3c
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-19 14:13:51 +0300

    !, !!

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-let a = 0;
-console.log(a++);
+let a = true;
 console.log(a);
-
-let b = 5;
-console.log(++b);
-
-let c = 3;
-console.log(c--);
-console.log(c);
-
-let d = 8;
-console.log(--d);
+console.log(!a);
+//!true = false
+//!!true = true
