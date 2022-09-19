b4dc5517c32e8c126ed8555daf1070ba7fda29a6
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-19 14:57:29 +0300

    Operators that change variables

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-true || true;
-true
-true || false;
-true
-false || false;
-false
+// let a = 42;
+// a += 3;
+// a -= 3;
+// a *= 3;
+// a /= 3;
+
+// let b=1, c=10 //Bad practice
+
+
+let a = 42;
+let b = 2;
+let c = 6;
+let d = 3;
+let e = 9;
+let f = 8;
+console.log(a*b);
+console.log(c/d);
+console.log(e+f);
+
+let g = 33;
+console.log(g);
+let l = false;
+console.log(l);
+let k = 'javasript';
+console.log(k);
+let m = 100;
+console.log(m);
+
+let num = 1;
+num++;
+num--;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+console.log(num);
