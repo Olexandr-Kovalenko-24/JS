5365e8aef305c9002b7f6c70b5ba651067467f4c
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-20 14:41:08 +0300

    Up

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const userDrink = prompt('Choose drink: \n1 - tea \n2 - coffe \n3 - juice');
 
 switch (userDrink) {
-    case '1':
+    case '1': {
         console.log('tea');
-    case '2':
+        break;
+    }
+    case '2': {
         console.log('coffe');
-    case '3':
+        break;
+    }
+    case '3': {
         console.log('juice');
-    case '4':
+        break;
+    }
+    case '4': {
         console.log('watter');
-    case '5':
+        break;
+    }
+    case '5': {
         console.log('lemonad');
+        break;
+    }
     default:
         console.log('Don`t no that drink');
 }
