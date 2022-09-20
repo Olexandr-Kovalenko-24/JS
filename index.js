25d0dd11fd99ec99b7e1ed44190119e8347b134d
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-20 16:55:16 +0300

    Task name resolve

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const UserAv = 'http://';
-const imPlaceholder = 'http://';
+// const UserAv = 'http://';
+// const imPlaceholder = 'http://';
 
 
-let imageSrc = UserAv ? UserAv : imPlaceholder;
+// let imageSrc = UserAv ? UserAv : imPlaceholder;
+
+
+let usname = prompt('What is your name');
+
+let user = usname ? console.log('Hello, ' + usname) : console.log('Hello, Anon');
+
+// if (usname) {
+//     console.log('Hello,' + usname);
+// } else {
+//     console.log('Hello, Anon'); 
+// };
