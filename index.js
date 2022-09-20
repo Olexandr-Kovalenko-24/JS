2cfab0fc7a992f29e255601fe8896a19278e9fa0
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-20 16:46:37 +0300

    Img task

--- index.js
+++ index.js
@@ -1,48 +1,5 @@
-// function square(b) {
-//     return b*b;
-// }
-
-// function supperFun (number, fn) { //HOF
-//     fn(number); // callback function
-// }
-
-// supperFun(5, square);
-
-let one = Number(prompt('write number'));
-let two = Number(prompt('write number2'));
-let operator = prompt('write operator');
-
-console.log(calculator(one, two, operator));
-
-function mul(a, b) {
-    return a * b;
-};
-
-function div(a, b) {
-    return a / b;
-};
-
-function min(a, b) {
-    return a - b;
-};
-
-function sum(a, b) {
-    return a + b;
-};
+const UserAv = 'http://';
+const imPlaceholder = 'http://';
 
 
-function calculator(firstNum, secNum, oper) {
-    switch (operator) {
-        case '*':
-            return mul(firstNum, secNum, oper);
-        case '/':
-            return div(firstNum, secNum, oper);
-        case '-':
-            return min(firstNum, secNum, oper);
-        case '+':
-            return sum(firstNum, secNum, oper);
-        default:
-            return 'Unknown value typed';
-    }
-}
-
+let imageSrc = UserAv ? UserAv : imPlaceholder;
