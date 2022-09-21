7a4cbfb124f7390d4e8071975c73c052c0115daa
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-21 14:53:25 +0300

    while, do while task

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-let i = 0;
-while (i < 10) {
-i++;
-console.log('1');
-}
+// let i = 0;
+// while (i < 10) {
+// i++;
+// console.log('1');
+// }
 
-let j = 0;
-do {
-    console.log('2')
-} while (j++ < 3);
+// let j = 0;
+// do {
+//     console.log('2')
+// } while (j++ < 3);
+
+
+// const password = 'Good password';
+// let UserValue = '';
+
+
+
+// do {
+//     let UserValue = prompt('Write password');
+// } while (UserValue !== password);
+// console.log('Good password');
+
+
+let n = 1;
+while (n++ < 10) {
+    if (n % 2 === 0) {
+        console.log(n);
+    }
+}
