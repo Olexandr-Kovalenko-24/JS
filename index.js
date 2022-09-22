2ac497a752a3323285b9833394f8f15c68722cf7
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-22 15:40:52 +0300

    short

--- index.js
+++ index.js
@@ -1,71 +1,77 @@
-let cat = {
-    breed: "Blue Cheshire",
-    name: 'Mussy',
-    age: 3,
-    weight: 10,
-    color: 'grey',
-    sleep: function () {
-        console.log('I am sleeping')
-    },
-    eat: function () {
-        console.log('I am eating')
-    }
-};
+// let cat = {
+//     breed: "Blue Cheshire",
+//     name: 'Mussy',
+//     age: 3,
+//     weight: 10,
+//     color: 'grey',
+//     sleep: function () {
+//         console.log('I am sleeping')
+//     },
+//     eat: function () {
+//         console.log('I am eating')
+//     }
+// };
 
-let catGirl = {
-    breed: "Blue Cheshire",
-    name: 'Jes',
-    age: 3,
-    weight: 8,
-    color: 'black',
-    sleep: function () {
-        console.log('I am sleeping')
-    },
-    eat: function () {
-        console.log('I am eating')
-    }
-};
+// let catGirl = {
+//     breed: "Blue Cheshire",
+//     name: 'Jes',
+//     age: 3,
+//     weight: 8,
+//     color: 'black',
+//     sleep: function () {
+//         console.log('I am sleeping')
+//     },
+//     eat: function () {
+//         console.log('I am eating')
+//     }
+// };
 
-cat.girlfriend = catGirl;
+// cat.girlfriend = catGirl;
 
 
-let windMy = {
-    color: "White",
-    open: function () {
-        console.log('open window')
-    },
-    weight: 15,
+// let windMy = {
+//     color: "White",
+//     open: function () {
+//         console.log('open window')
+//     },
+//     weight: 15,
+// }
+// let glass = {
+//     color: "Opaque",
+//     weight: 7,
+//     width: 1000,
+//     300: 'Sparta',
+// }
+// windMy.glass = glass;
+
+
+// let variable = 'color';
+// let num = 2;
+
+// cat[num];
+// cat[variable];
+
+
+
+
+// const weekPlan = {
+//     Mon: 'dantist',
+//     Tue: 'go to park',
+//     Wed: 'meet friends',
+//     Thu: 'learn',
+//     Fri: 'hard work',
+//     Sat: 'go to party'
+// }
+
+// function getWeekPlan (plan) {
+//     const userValue = prompt('Write the day of week');
+//     alert(plan [userValue])
+// }
+
+// getWeekPlan(weekPlan);
+
+let property = 1;
+let obj = {
+    key: 'val',
+    property,
 }
-let glass = {
-    color: "Opaque",
-    weight: 7,
-    width: 1000,
-    300: 'Sparta',
-}
-windMy.glass = glass;
-
-
-let variable = 'color';
-let num = 2;
-
-cat[num];
-cat[variable];
-
-
-
-
-const weekPlan = {
-    Mon: 'dantist',
-    Tue: 'go to park',
-    Wed: 'meet friends',
-    Thu: 'learn',
-    Fri: 'hard work',
-    Sat: 'go to party'
-}
-
-function getWeekPlan (plan) {
-    const userValue = prompt('Write the day of week');
-    alert(plan [userValue])
-}
-
-getWeekPlan(weekPlan);
