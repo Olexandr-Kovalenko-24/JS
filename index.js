54ab20704059102d38e761c810acaf092b4db280
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-22 14:29:20 +0300

    New obj

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-let obj = {
-    key: 'value',
-    'key2': 'value2',
-    key3: 'value',
-    key3: 'New value',
-    fun: function () {
-        console.log('I am method')
+let cat = {
+    breed: "Blue Cheshire",
+    name: 'Mussy',
+    age: 3,
+    weight: 10,
+    color: 'grey',
+    sleep: function () {
+        console.log('I am sleeping')
+    },
+    eat: function () {
+        console.log('I am eating')
     }
-}
+};
 
-let newObj = obj;
+let windowBeside = {
+    color: 'white',
+    price: 'a lot of',
+    width: 1200,
+    height: 1200,
+    open: function () {
+        console.log('Open window')
+    },
+    close: function () {
+        console.log('Close window')
+    },
+}
