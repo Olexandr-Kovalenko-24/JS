7862175a71231f45ab58e329372db2cb7dc2c926
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-22 16:01:30 +0300

    Res task resolve

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-let user = {
-    firstName: 'Alex',
-    lastName: 'Kovalenko',
-    age: 24,
-    email: '22.com',
+// let user = {
+//     firstName: 'Alex',
+//     lastName: 'Kovalenko',
+//     age: 24,
+//     email: '22.com',
+// }
+
+// // console.log(user.age)
+
+// for (let key in user) {
+//     console.log(key +' '+ user[key])
+// }
+
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 5,
+    d: 7,
+    e: 9,
 }
-
-// console.log(user.age)
-
-for (let key in user) {
-    console.log(key +' '+ user[key])
+let res = 0;
+for (let key in obj) {
+    res += obj[key]
+    console.log(res)
 }
