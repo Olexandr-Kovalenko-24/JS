a130467e990879ecc0700bcf8b066bad21dd15aa
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-23 15:06:26 +0300

    Task car resolve myself

--- index.js
+++ index.js
@@ -29,3 +29,32 @@ this.sayHello = function() {
 
 const us = new User('Alex', 'Kov', 40);
 const us2 = new User('Jack', 'Sparrow', 30);
+
+
+
+function Car(brand, model, capasityOfEngine, numberOfSeats, topSpeed, currentSpeed) {
+    this.brand = brand;
+    this.model = model;
+    this.capasityOfEngine = capasityOfEngine;
+    this.numberOfSeats = numberOfSeats;
+    this.topSpeed = topSpeed;
+    this.currentSpeed = currentSpeed;
+    this.run = function () {
+        console.log(this.model + ' rides')
+    };
+    this.stop = function () {
+        console.log(this.model + ' stop')
+    };
+    this.accelerate = function (value) {
+        let acc = this.currentSpeed + value;
+        if (acc >= this.topSpeed) {console.log(this.topSpeed)}
+        else {console.log(acc)}
+    };
+    this.deaccelerate = function (value) {
+        let acc = this.currentSpeed - value;
+        if (acc > 0) {console.log(acc)}
+        else {console.log(this.stop())}
+    };
+}
+
+const car1 = new Car('lexus', 'supper', 2000, 5, 300, 100);
