987ed0c17de10765763c05335823fbac97af0dac
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-23 14:41:33 +0300

    Constructor function

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 'use strict'
 
-const user1 = {
-    firstName: 'Alex',
-    lastName: 'Grow',
-    age: 30,
-    getName: function () {
-        console.log(this.firstName + this.lastName);
-    }
+// const user1 = {
+//     firstName: 'Alex',
+//     lastName: 'Grow',
+//     age: 30,
+//     getName: function () {
+//         console.log(this.firstName + this.lastName);
+//     }
+// }
+
+// const user2 = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     getName: function () {
+//         console.log(this.firstName + this.lastName);
+//     }
+// }
+
+function User(firstName, lastName, age) {
+this.firstName = firstName;
+this.lastName = lastName;
+this.age = age;
+this.sayHello = function() {
+    console.log(this.firstName + ' say: "Hello"')
+}
 }
 
-const user2 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    getName: function () {
-        console.log(this.firstName + this.lastName);
-    }
-}
-
-function fn () {
-    console.log(this)
-}
+const us = new User('Alex', 'Kov', 40);
+const us2 = new User('Jack', 'Sparrow', 30);
