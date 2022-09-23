05b0ed56fdf40b83932d981477d6115f3f3550d5
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-23 16:04:49 +0300

    User task resolve

--- index.js
+++ index.js
@@ -18,44 +18,67 @@
 //     }
 // }
 
-function User(firstName, lastName, age) {
-this.firstName = firstName;
-this.lastName = lastName;
-this.age = age;
-this.sayHello = function() {
-    console.log(this.firstName + ' say: "Hello"')
-}
-}
+// function User(firstName, lastName, age) {
+// this.firstName = firstName;
+// this.lastName = lastName;
+// this.age = age;
+// this.sayHello = function() {
+//     console.log(this.firstName + ' say: "Hello"')
+// }
+// }
 
-const us = new User('Alex', 'Kov', 40);
-const us2 = new User('Jack', 'Sparrow', 30);
+// const us = new User('Alex', 'Kov', 40);
+// const us2 = new User('Jack', 'Sparrow', 30);
 
 
 
-function Car(brand, model, capasityOfEngine, numberOfSeats, topSpeed) {
-    this.brand = brand;
-    this.model = model;
-    this.capasityOfEngine = capasityOfEngine;
-    this.numberOfSeats = numberOfSeats;
-    this.topSpeed = topSpeed;
-    this.currentSpeed = 0;
-    this.run = function () {
-        console.log(this.model + ' rides ' + this.currentSpeed + ' km/h')
+// function Car(brand, model, capasityOfEngine, numberOfSeats, topSpeed) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.capasityOfEngine = capasityOfEngine;
+//     this.numberOfSeats = numberOfSeats;
+//     this.topSpeed = topSpeed;
+//     this.currentSpeed = 0;
+//     this.run = function () {
+//         console.log(this.model + ' rides ' + this.currentSpeed + ' km/h')
+//     };
+//     this.stop = function () {
+//         console.log(this.model + ' is stopped')
+//     };
+//     this.accelerate = function (value) {
+//         this.currentSpeed += value;
+//         if (this.currentSpeed > this.topSpeed) {this.currentSpeed = this.topSpeed}
+//             return this.run()
+//     };
+//     this.deaccelerate = function (value) {
+//         this.currentSpeed -= value;
+//         if (this.currentSpeed > 0) {console.log(this.currentSpeed)}
+//         else {console.log(this.stop())}
+//     };
+// }
+
+// const lexusLX500 = new Car('lexus', 'LX 500', 2000, 5, 300);
+
+
+
+
+function User(name, lastName, age, email) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.email = email;
+    this.password = 123;
+    this.isLogin = false;
+    this.getFullName = function () {
+        console.log(this.name + ' ' + this.lastName)
     };
-    this.stop = function () {
-        console.log(this.model + ' is stopped')
-    };
-    this.accelerate = function (value) {
-        this.currentSpeed += value;
-        if (this.currentSpeed > this.topSpeed) {this.currentSpeed = this.topSpeed}
-            return this.run()
-    };
-    this.deaccelerate = function (value) {
-        this.currentSpeed -= value;
-        if (this.currentSpeed > 0) {console.log(this.currentSpeed)}
-        else {console.log(this.stop())}
-    };
-}
+    this.signIn = function () {
+        let pass = Number(prompt('Write your password'));
+        if (this.password === pass) {
+            this.isLogin = true;
+            return 'you are logIn'
+        } else {return 'password incorect'}
+    }
+};
 
-const lexusLX500 = new Car('lexus', 'LX 500', 2000, 5, 300);
-
+const alex = new User ('Alex', 'Kovalenko', 24, '22.gmailcom')
