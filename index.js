769532e80548d4018144ffd61c64e6e36d9277e2
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-26 15:00:56 +0300

    Array task resolve

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 'use strict'
 
-const arr = [12, 23, 6, 78, 45, 76, 86, 32, 89, 21]
+const arr = [12, 23, 6, 79, 45, 76, 86, 32, 89, 21];
 
-function sumArray (array){
-return array[2]+array[4]
-};
+// function sum3and5element (array){
+// return array[2]+array[4]
+// };
+
+function sumOfArray (array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+function avarageOfArray (array) {
+    let sum = sumOfArray(array);
+        return sum / array.length;
+}
