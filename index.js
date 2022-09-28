90d336e9a4357e3e6de4266eb9cf7621a81f4fcf
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-28 16:52:41 +0300

    task forEach resolve

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict'
 
-function User(firstName, lastName, age, mail, isSubscribe = false) {
+const NATIONALITIES = [
+    'Ukraine',
+    'Great Britain',
+    'Germany',
+    'France',
+    'Spain'
+];
+
+function User(firstName, lastName, age, mail, isSubscribe = false, nationality) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
     this.mail = mail;
     this.isSubscribe = isSubscribe;
+    this.nationality = nationality;
 }
 
 function getRandomArbitrary(min, max) {
@@ -16,7 +25,8 @@ function createArrayOfUsers(quantity) {
     const userArr = []
     for (let i = 0; i <= quantity; i++) {
         let us = new User(`Name ${i}`, `LastName ${i}`, getRandomArbitrary(1, 100),
-            `mail${1}.com`, Boolean(Math.round(Math.random())))
+            `mail${1}.com`, Boolean(Math.round(Math.random())),
+            NATIONALITIES[getRandomArbitrary(0,4)])
         userArr.push(us);
     }
     return userArr;
@@ -34,8 +44,28 @@ arrUsers.sort(function(usA, usB){
 return usA.age - usB.age
 })
 
-console.table(arrUsers);
+// console.table(arrUsers);
 
 const filtered = arrUsers.filter(function (elem) {
-    return elem.age > 20
+    return elem.age >= 18 && elem.isSubscribe
 })
+
+const mail = arrUsers
+.filter(function (elem) {
+    return elem.age >= 18 && elem.isSubscribe
+}).map(function (user) {
+    return user.mail
+})
+
+const fullNameOfUkrainian = arrUsers
+.filter(function(user){
+    return (user.nationality === 'Ukraine') && user.isSubscribe
+}).map(function(us){
+    return `${us.firstName} ${us.lastName}`
+})
+// console.table(fullNameOfUkrainian);
+
+arrUsers.forEach (function(user){
+    return user.isSubscribe = true;
+})
+console.table(arrUsers);
