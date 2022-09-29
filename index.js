3809f01d837afa99c70e338b3fb7e3862267c6b1
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-29 14:55:26 +0300

    Rest

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 'use strict'
 
-const newspaper = {
-    title: 'Yellow Press',
-    articles: [
-        {
-            aithor: 'John Doe',
-            text: 'Blablabla',
-            date: 'today',
-        },
-        {
-            aithor: 'Alex Doe',
-            text: 'Blablabla',
-            date: 'today',
-        },
-        {
-            aithor: 'Michael Doe',
-            text: 'Blablabla',
-            date: 'today',
-        },
-        {
-            aithor: 'Jo Doe',
-            text: 'Blablabla',
-            date: 'today',
-        },
-    ],
-    showArticles: function (){
-        this.articles.forEach((article)=>{
-            console.log(`${this.title}, ${article.aithor}: ${article.text}`)
-        });
-    }
+function sum(a, b, ...array){
+    console.log(a);
+    console.log(b);
+    console.log(array);
 }
+
+sum(1,2,35,63,24)
