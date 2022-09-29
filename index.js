3df0ac676467c0827d867c1023b282b2ebbd50fe
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-29 15:12:34 +0300

    spred

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 'use strict'
 
-function sum(a, b, ...array){
-    console.log(a);
-    console.log(b);
-    console.log(array);
+function sum(a, b, c){
+    return a+b+c;
 }
 
-sum(1,2,35,63,24)
 
+const numbers = [1, 2, 3, 75, 23]
 
-const sumOfAllArguments = (...restAr) => 
-restAr.reduce((accumulator, currentValue) => 
-accumulator + currentValue, 0)
-
-console.log(sumOfAllArguments(3,46,24,67))
+console.log(sum(...numbers));
