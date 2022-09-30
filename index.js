59229d9ed1ef6f63467cf1b53b5651aac6836fd9
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-30 17:14:10 +0300

    All task resolved

--- index.js
+++ index.js
@@ -13,17 +13,49 @@ console.log(text.replaceAll('!', ' ').trim());
 
 
 
-function toUpFirstLater(string){
+function toUpFirstLater(string) {
     return string.at(0).toUpperCase()
-    .concat(string.slice(1));
+        .concat(string.slice(1));
 
 }
 
 
-function checkSpam(string){
+function checkSpam(string) {
     if (string.includes('viagra')
-    || string.includes('XXX')
-    || string.includes('buy'))
-    {return true} else{return false}
+        || string.includes('XXX')
+        || string.includes('buy')) { return true } else { return false }
 }
 
+function truncate(string, maxLength) {
+    if (string.length > maxLength) {
+        return string.slice(0, maxLength).concat('...')
+    } else { return string }
+}
+
+
+function palindrom(string) {
+    return string.toLowerCase().split('').reverse().join('')
+        === string.toLowerCase()
+}
+
+
+function vowelQuantityInString(string) {
+    let counter = 0;
+    const vowel = ['a', 'e', 'i', 'o', 'u'];
+    for (let i = 0; i < string.length; i++) {
+        for (let j = 0; j < vowel.length; j++) {
+            if (string[i] === vowel[j]) {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
+
+// function vowelQuantityInString2(str) {
+//     const vowel = ['a', 'e', 'i', 'o', 'u'];
+//     return str.split('').filter((letter) => vowel.includes(letter)).length
+// }
+
+const vowelQuantityInString2 = (str) => str.split('').filter((letter) => ['a', 'e', 'i', 'o', 'u'].includes(letter)).length
