f4869e41e893ebef50e0aeae43ac9f950d5d2795
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-09-30 15:20:40 +0300

    Task resolve string methods

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 'use strict'
 
-const a = 'hello';
-console.log(a.endsWith('o'));
-console.log(a.indexOf('l'));
-console.log(a.repeat('3'));
-console.log(a.replace('el', 'ko'));
-console.log(a.());
+// const a = 'hello';
+// console.log(a.endsWith('o'));
+// console.log(a.indexOf('l'));
+// console.log(a.repeat('3'));
+// console.log(a.replace('el', 'ko'));
+// console.log(a.());
+
+
+const text = 'Я!вчу!джаваскрипт!';
+console.log(text.replaceAll('!', ' ').trim());
+
+
+
+function toUpFirstLater(string){
+    return string.at(0).toUpperCase()
+    .concat(string.slice(1));
+
+}
