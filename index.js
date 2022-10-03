cb1c2c86afaaae6285756ecfd7686669901ba5b9
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-03 15:55:06 +0300

    task resolve

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 'use strict'
 
-function factorial (number){
+function factorial(number) {
     if (number === 1) {
         return 1;
     }
-    return number * factorial (number -1);
+    return number * factorial(number - 1);
 }
 
-function pow (base, power) {
+function pow(base, power) {
     if (power === 1) {
         return base;
-    } else {
-        return base * pow(base, power - 1)
     }
-    
+    return base * pow(base, power - 1)
 }
+
+// const pow2 = (base, power) => (power === 1) ? base :
+// pow2(base, power - 1)
+
+
+const arr = [2, 3, 2, 1, [4, 2, 3, [6, 3, 2, [3], 3, 3], 2]]
+
+function sumOfNumbers(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            sum += sumOfNumbers(array[i]);
+        } else {
+            sum += array[i]
+        }
+    }
+    return sum;
+}
+
+console.log(sumOfNumbers(arr))
