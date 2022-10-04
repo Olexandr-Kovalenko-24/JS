467feeada7935a494bfb9ccf39e6539cd2557caa
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-04 14:58:12 +0300

    Class

---
+++ src/new_syntax.js
@@ -0,0 +1,32 @@
+class MyNewArray {
+    constructor(){
+        this.length = 0;
+    }
+    push () {
+        for (let i = 0; i < arguments.length; i++){
+            this[this.length] = arguments[i];
+            this.length++;
+        }
+        return this.length;
+    }
+    pop () {
+        const lastItem = this[this.length-1];
+        delete this[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+    forEach () {
+        for(let i = 0; i < this.length; i++){
+            callbackFunction(this[i], i, this);
+    }
+}
+    map () {
+        const arr = new MyArray();
+        for(let i = 0; i < this.length; i++){
+        arr.push(callbackFunction(this[i]));
+        };
+        return arr;
+    }
+}
+
+const narr = new MyNewArray
