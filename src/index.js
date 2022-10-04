5688519b6d5a3911c2ead08460dc9a1aac7e8a5c
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-04 15:07:15 +0300

    Task new class User resolve

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 'use strict'
 
+class User {
+    constructor(name, lastName, age, email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+    getFullName () {
+        return `${this.name} ${this.lastName}`
+    }
+    sayHello () {
+        return `Hello, ${this.name}`
+    }
+}
 
-
+const Alex = new User('Alex', "Kovalenko", 24, '22@gmail.com')
 
 
