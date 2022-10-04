598729af6c625ebc3b88f9e6afd5d2d64f9dafcb
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-04 14:47:04 +0300

    Try catch

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ function sayHello(name) {
 }
 
 
+/**
+ * 
+ * @param {Number} base 
+ * @param {Number} power - More than 0
+ * @returns {Number} base on power
+ * @throws {TypeError} base or power not a number
+ * @throws {RangeError} when power < 0
+ */
+
 function pow (base, power) {
     if (typeof base !== 'number' || typeof power !== 'number') {
         throw new TypeError('Base and power must be a number')
@@ -27,5 +36,16 @@ function pow (base, power) {
     }
     return res;
 }
+
+console.log('start')
+try {
+    pow(2,-2)
+    console.log('end')
+} catch(error){
+    console.log(error)
+}
+
+console.log('code is alive')
+
 
 
