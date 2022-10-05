9bdaf993fc5a143cdd0d97a09ab05971a77f8620
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-05 14:45:42 +0300

    task with get/set/class resolve (from,to)

--- src/index.js
+++ src/index.js
@@ -113,3 +113,47 @@ class Worker {
         return this.rate * this.days;
     }
 }
+
+class Validator {
+    constructor (from, to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    set from(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('from must be a number')
+        }
+        this._from = value;
+    }
+
+    get rate () {
+        return this._from;
+    }
+
+    set to(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('to must be a number')
+        }
+        this._to = value;
+    }
+
+    get to () {
+        return this._to;
+    }
+
+    get range () {
+        let array = [];
+        for (let i = this._from; i <= this._to; i++){
+            array.push(i);
+        }
+        return array;
+    }
+
+    isNumberInRange (number) {
+        if (this.range.includes(number)) {return true}
+        else {return false}
+    }
+}
+
+const val = new Validator(5,9)
