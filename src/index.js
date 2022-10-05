af67fff4c0f23261bf754372cbdc2df3969ab475
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-05 16:34:28 +0300

    task resolve Figure

--- src/index.js
+++ src/index.js
@@ -204,3 +204,48 @@ class Kitten extends Cat {
         return `${this.name} is noizy sleeping`
     }
 }
+
+
+class Figure {
+    constructor (type) {
+        this.sideQuantity = type;
+    }
+    getArea () {
+
+    }
+}
+
+class Triangle extends Figure {
+    constructor(a,b, angle){
+        supper(3);
+        this.a = a;
+        this.b = b;
+        this.angle = angle;
+    }
+
+    getArea (){
+        return 0.5*this.a*this.b*Math.sin(this.angle)
+    }
+}
+
+class Square extends Figure {
+    constructor(a){
+        super(4);
+        this.a = a;
+    }
+
+    getArea () {
+        return this.a*this.a
+    }
+}
+
+class Circle extends Figure {
+    constructor(d){
+        super(Infinity);
+        this.d = d;
+    }
+
+    getArea () {
+        return Math.PI*(this.d*this.d /4)
+    }
+}
