65e9aaadca5ae0f7cd725efa03380370f9f78a00
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-12 15:22:18 +0300

    Recursive binary

--- src/index.js
+++ src/index.js
@@ -1,55 +1,38 @@
 'use strict'
 
+const arr = [2,3,4,5,6,7,8,9,10]
 
-class Student {
-    constructor(name, lastName, yearIn){
-        this.name = name;
-        this.lastName = lastName;
-        this.yearIn = yearIn;
-    }
 
-    getCourse () {
-    new Date().getFullYear()- this.yearIn
+function binarySearchFunction(array, searchValue){
+    let start = 0;
+    let end = array.length - 1;
+    let middle = Math.round((start+end)/2);
+
+    while(true) {
+        if (searchValue === array[middle]) {
+            return middle;
+        }
+        if (searchValue > array[middle]){
+            start = middle+1;
+            middle = Math.floor((start+end)/2);
+        } else {
+            end = middle-1;
+            middle = Math.ceil((start+end)/2);
+        }
     }
 }
 
-function getSeason(){
-    const month = new Date().getMonth();
-    if(month <= 1 || month === 11){
-        return 'winter'
-    } else if (month >=2 && month <=4){
-        return 'spring'
-    } else if (month >=5 && month <=7) {
-        return 'summer'
-    } else { 
-        return 'fall'
+function recursivebinary(array, searchValue){
+    const middle = Math.ceil((array.length-1)/2);
+
+    if (array[middle] === searchValue) {
+        return middle;
+    } else if (array[middle] > searchValue) {
+        return recursivebinary (array.slice(0, middle), searchValue);
     }
-} 
-
-
-function getSeason2(){
-    const month = new Date().getMonth();
-switch(month) {
-    case 0:
-    case 1:
-    case 11:
-        return 'winter';
-    case 2:
-    case 3:
-    case 4:
-        return 'spring';
-    case 5:
-    case 6:
-    case 7:
-        return 'summer';
-    case 8:
-    case 9:
-    case 10:
-        return 'fall';
-    default:
-        return null;
+    else if (array[middle] < searchValue) {
+        return middle + recursivebinary (array.slice(middle), searchValue);
+    } else {
+        return -1;
+    }
 }
-}
-
-const date = new Date()
-console.log (`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`)
