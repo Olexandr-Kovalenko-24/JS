9274bc98a6a4a41c7702d52861111fcc25feff1d
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-13 14:57:47 +0300

    Queue

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 'use strict'
 
+class Queue {
+    constructor(){
+        this._head = 0;
+        this._tail = 0;
+    }
+
+    get size (){
+        return this._tail - this._head;
+    }
+
+    enqueue(value){
+        this[`_${this._tail}`] = value;
+        this._tail++;
+
+        return this.size
+    }
+
+    dequeue(){
+        if(this.size) {
+            const firstItem = this[`_${this._head}`];
+            delete this[`_${this._head}`];
+            this._head++
+            return firstItem;
+        }
+    }
+}
