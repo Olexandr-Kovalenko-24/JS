9534b69f7419a8c9b2411c3e0a7fa0dacd1e2fc4
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-13 15:42:48 +0300

    Merge Queue task resolve

--- src/Queue.js
+++ src/Queue.js
@@ -1,31 +1,47 @@
 'use strict'
 
 class Queue {
-    constructor(...arr){
+    constructor(...args) {
         this._head = 0;
         this._tail = 0;
-        for (const item of arr) {
-            this.enqueue(item);
+        for (const value of args) {
+            this.enqueue(value);
         }
     }
 
-    get size (){
+    get size() {
         return this._tail - this._head;
     }
 
-    enqueue(value){
+    enqueue(value) {
         this[`_${this._tail}`] = value;
         this._tail++;
 
         return this.size
     }
 
-    dequeue(){
-        if(this.size) {
+    dequeue() {
+        if (this.size) {
             const firstItem = this[`_${this._head}`];
             delete this[`_${this._head}`];
             this._head++
             return firstItem;
         }
     }
+}
+
+
+function mergeQueues(q1, q2) {
+    const mergeQ = new Queue();
+    const maxLength = q1.size > q2.size ? q1 : q2;
+    while (maxLength.size) {
+        if (q1.size) {
+            resQueue.enqueue(q1.dequeue())
+        }
+        if (q2.size) {
+            resQueue.enqueue(q2.dequeue())
+        }
+    }
+
+    return resQueue;
 }
