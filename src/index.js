7c0013675ea4c129c9934f520b2fdae9f3175a51
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-14 17:12:42 +0300

    Array desctruction and other species

--- src/index.js
+++ src/index.js
@@ -69,6 +69,56 @@ function consoleValue({target: {value, name}}){
 }
 
 
-function getFullName({name: {first, last}}){
+function getFullName({name: {first, last}, ...restOfUser}){
+    console.log(restOfUser)
     return `${first} ${last}`
 }
+
+
+
+
+const arr = [1,2,3,4,5,6];
+
+const [firstElem, secondElem,,,fiveElement] = arr;
+
+const [firstE, ...restArr] = arr;
+
+function getStringOfCortege({first, second}){
+    return `${first} - ${second}`
+}
+
+// const map = new Map([[1, {username: 'john doe'}], 
+// [2, {username: 'janedoe'}], [3, {username: 
+// 'alexdoe'}]]);
+// [...map.entries()].map((cortege)=>{
+//     return `${cortege[0]} - ${cortege[1].username}`
+// })
+
+[...map.entries()].map(([key, {username}])=>{
+    return `${key} - ${username}`
+})
+
+const settings = {
+    width: 200,
+    height: 400,
+    on: true,
+}
+
+function getSet({width, height, on = false}){
+    console.log(`${width} ${height} ${on}`)
+}
+
+const us = {
+    name: {
+        first: 'Ricky'
+    },
+    passw: '1234'
+}
+
+
+const {
+    name: {
+        first: userFirstName, 
+        last: userLastName = 'Anonumus'
+    }, 
+    passw = 'qwerty'} = us;
