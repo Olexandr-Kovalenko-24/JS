85f39117cecf35b1e5f41ef17fb8f12b1cf4a3b8
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-14 14:47:51 +0300

    Map compareString task

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ vocabulary.set('meat', `м'ясо`);
 
 
 
-function translater(str,vocabulary){
+function translater(str, vocabulary) {
     const wordArray = str.toLowerCase().split(' ');
     const resArray = [];
     for (const word of wordArray) {
-        if(vocabulary.has(word)){
+        if (vocabulary.has(word)) {
             resArray.push(vocabulary.get(word));
         } else {
             resArray.push(word);
@@ -45,6 +45,30 @@ mapMessage.set(user1, johnMassage);
 mapMessage.set(user2, janeMassage);
 
 
+function createVoc(str) {
+    const map = new Map();
+    for (const symb of str1) {
+        if (map.has(symb)) {
+            const currValue = map.get(symb)
+            map.set(symb, currValue + 1)
+        } else {
+            map.set(symb, 1)
+        }
+    }
+}
 
+function compareString(str1, str2) {
+    const map1 = createVoc(str1);
+    const map2 = createVoc(str2);
 
+    if (map1.size !== map2.size) {
+        return false
+    }
 
+    for (const key of map1.keys()) {
+        if (map1.get(key) !== map2.get(key)) {
+            return false
+        }
+    }
+    return true
+}
