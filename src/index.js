eb346f3abc9c987af207ed4b724ccc23e87a4f20
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-14 15:28:16 +0300

    task resolve

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ const arr2 = [4,2,7,10];
 const set1 = new Set(arr1.concat(arr2))
 const res = [...new Set([...arr1, ...arr2])]
 
+
+
+const map = new Map([[1, {username: 'john doe'}], 
+[2, {username: 'janedoe'}], [3, {username: 
+'alexdoe'}]]);
+
+ 
+const resTask = [...map.entries()].map((cortege)=>{
+    return `${cortege[0]} - ${cortege[1].username}`
+})
